0a77acac39439914a7ed33e9ff9f6d83ea1ba76b
Salem Nassar <salemnassar5@gmail.com>
2023-02-02 15:36:23 -0600

    Add Files

---
+++ main.py
@@ -0,0 +1,18 @@
+from treelib import Node, Tree
+
+goalState = [
+    ['7', '8', '1'],
+    ['6', '*', '2'],
+    ['5', '4', '3']
+]
+
+initialState = [
+    ['6', '7', '1'],
+    ['8', '2', '*'],
+    ['5', '4', '3']
+]
+
+tree = Tree()
+tree.create_node(initialState, tree.size())
+tree.create_node(1, tree.size(), parent=tree.size() - 1)
+tree.show()
